9f51beb96f569130e5a2ca5358fd3225d66fb2f4
Aribel Ruiz <aribelruiz@knights.ucf.edu>
2023-09-05 16:15:33 -0400

    Implemented Likes in the backend

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,8 @@ const commentsRouter = require('./routes/Comments');
 app.use("/comments", commentsRouter);
 const usersRouter = require('./routes/Users');
 app.use("/auth", usersRouter);
+const likesRouter = require('./routes/Likes');
+app.use("/likes", likesRouter);
 
 
 const sequelize = new Sequelize("mysql://root:Pa$$word3!@localhost:3306/fullstackpractice");
