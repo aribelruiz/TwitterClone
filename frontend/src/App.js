32e1c775d2b64b9ea19609ceab64a44361cc6c72
Aribel Ruiz <aribelruiz@knights.ucf.edu>
2023-09-12 18:39:06 -0400

    Updated Nav CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import './App.scss';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import Home from './Components/Home/Home';
-import CreatePost from './Components/CreatePost/CreatePost';
 import Post from './Components/Post/Post';
 import Login from './Components/Login/Login';
 import Register from './Components/Register/Register';
@@ -13,6 +12,19 @@ import PageNotFound from './Components/PageNotFound/PageNotFound';
 import { AuthContext } from './helpers/AuthContext';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+
+// Import nav icons
+import { HomeOutlined as HomeIcon, 
+        Search as SearchIcon, 
+        NotificationsOutlined as NotifcationsIcon, 
+        EmailOutlined as MessageIcon,
+        PersonOutlined as ProfileIcon,
+        PendingOutlined as MoreIcon,
+        PostAddOutlined as PostIcon
+      } from '@mui/icons-material';
+
+import logoIcon from './Images/aribellogo.png';
+
 
 function App() {
 
@@ -48,33 +60,71 @@ function App() {
       <AuthContext.Provider value={{ authState, setAuthState }}>
         <Router>
           <div className='navbar'>
+            <img className='nav-link' id='logo-icon' alt='logo' src={logoIcon}/>
             {!authState.status ? (
               <>
                 <Link className='nav-link' to="/login"> Login </Link>
                 <Link className='nav-link' to="/register"> Register </Link>
               </>
             ) : (
-              <>
-                <Link className='nav-link' to="/"> Home Page </Link>
-                <div className='nav-right'>
-                  <p className='username' onClick={() => window.location.replace(`/profile/${authState.id}`)}>
-                    {authState.username}
-                  </p>
-                  <button onClick={logout}> Logout </button>
+              <div className='nav-icons'>
+                <div className='nav-body'>
+                  <Link className='nav-link' to="/"> 
+                    <HomeIcon className='nav-icon'/>
+                    <h3>Home</h3>
+                  </Link>
+                  <Link className='nav-link' to="/"> 
+                    <SearchIcon className='nav-icon'/>
+                    <h3>Explore</h3>
+                  </Link>
+                  <Link className='nav-link' to="/"> 
+                    <NotifcationsIcon className='nav-icon'/>
+                    <h3>Notifications</h3>
+                  </Link>
+                  <Link className='nav-link' to="/"> 
+                    <MessageIcon className='nav-icon'/>
+                    <h3>Messages</h3>
+                  </Link>
+                  <Link className='nav-link' to={`/profile/${authState.id}`}> 
+                    <ProfileIcon className='nav-icon'/>
+                    <h3>Profile</h3>
+                  </Link>
+                  <Link className='nav-link' to="/"> 
+                    <MoreIcon className='nav-icon'/>
+                    <h3>More</h3>
+                  </Link>
+                  <Link className='nav-link post-btn' to="/"> 
+                    <PostIcon className='nav-icon post-icon'/>
+                    <h3>Post</h3>
+                  </Link>
                 </div>
-              </>
+
+                <div className='nav-footer'>
+                  <div className='nav-profile'>
+                    <img className='profile-img' src={logoIcon} alt='profile-img'></img>
+                    <p className='username' onClick={() => window.location.replace(`/profile/${authState.id}`)}>
+                      {authState.username}
+                    </p>
+                  </div>
+                  <button onClick={logout}> 
+                    Logout 
+                  </button>
+                </div>
+              </div>
             )}
           </div>
-
-          <Routes>
-            <Route exact path='/' element={<Home/>}/>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/register' element={<Register/>}/>
-            <Route path='/profile/:id' element={<Profile/>}/>
-            <Route path='/edit-profile' element={<EditProfile/>}/>
-            <Route path='/post/:id' element={<Post/>}/>
-            <Route path="*" element={<PageNotFound/>} />
-          </Routes>
+          
+          <div className='main-page'>
+            <Routes>
+              <Route exact path='/' element={<Home/>}/>
+              <Route path='/login' element={<Login/>}/>
+              <Route path='/register' element={<Register/>}/>
+              <Route path='/profile/:id' element={<Profile/>}/>
+              <Route path='/edit-profile' element={<EditProfile/>}/>
+              <Route path='/post/:id' element={<Post/>}/>
+              <Route path="*" element={<PageNotFound/>} />
+            </Routes>
+          </div>
         </Router>
       </AuthContext.Provider>
     </div>
